54eb22ea61214ff2ed1e1d6202d93bd275b20ec7
zahra-22 <zahraaden22@hotmail.com>
2025-12-05 14:43:40 -0500

    Updated .gitignore and final project submission

--- cypress.config.js
+++ cypress.config.js
@@ -2,7 +2,7 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
-    experimentalStudio: true,
+    experimentalStudio: true,   // <-- ADD THIS LINE
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
